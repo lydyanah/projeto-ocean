651db82e0f2e0548dc4c3fae35fe84e5c0116434
lydyanah <lidiana.tdsc@gmail.com>
2021-06-16 10:47:22 -0300

    itens map

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const itens = [
+  {
+    nome: "Gollum",
+    imagem: "/assets/gollum.jpg",
+  },
+  {
+    nome: "Galadriel",
+    imagem: "/assets/galadriel.jpg",
+  },
+  {
+    nome: "Legolas",
+    imagem: "/assets/legolas.jpg",
+  },
+];
+
 function CardItem(props) {
   return (
     <div className="card_item">
-      <h2>{props.nome}</h2>
-      <img src={props.imagem} alt="Imagem do Personagem" width="300" />
+      <h2>{props.item.nome}</h2>
+      <img
+        src={props.item.imagem}
+        alt="Imagem do Personagem"
+        width="300"
+        height="200"
+      />
     </div>
   );
 }
@@ -14,14 +34,9 @@ function CardItem(props) {
 function ListaItens() {
   return (
     <div className="lista_itens">
-      <CardItem
-        nome="Golum"
-        imagem="https://static3.tcdn.com.br/img/img_prod/460977/pre_venda_estatua_gollum_o_senhor_dos_aneis_the_lord_of_the_rings_masters_collection_escala_1_3_weta_37337_9_20201211173419.jpg"
-      />
-      <CardItem
-        nome="Galadriel"
-        imagem="https://static.wikia.nocookie.net/lotr/images/a/af/Galadriel_-_ROTK.png"
-      />
+      {itens.map((item) => (
+        <CardItem item={item} />
+      ))}
     </div>
   );
 }
